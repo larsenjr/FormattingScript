74e69bb725150dce9dde1ba1a5c82893a3b92b42
Stian Larsen <stian916@gmail.com>
2020-06-06 21:18:44 +0200

    Create UpdateScript.ps1

---
+++ UpdateScript.ps1
@@ -0,0 +1,28 @@
+﻿# UpdateScript
+#
+#
+# 05.06.2020
+# Stian Larsen
+
+
+$isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+
+    if ($isAdmin -eq $False) {
+        Write-Host "You need to start this script as admin!" "`n"
+        Start-Sleep -Seconds 2
+        $InstallationPath = Get-Location
+        Start-Process powershell -Verb runAs
+    }
+
+    Write-host "Updating applications.. Please Wait." -ForegroundColor Yellow
+
+    choco update all -a --verbose
+
+
+    Write-host "Upgrading applications.. Please wait" -ForegroundColor Yellow
+    choco upgrade all -a --verbose 
+   
+
+   if ($UpdatedPrograms -eq $True) {
+        Out-Null
+   }
