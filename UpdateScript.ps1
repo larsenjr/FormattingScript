8b93aa731df1590d9be54f1725a57d480ff75ad0
Stian Larsen <stian916@gmail.com>
2020-06-24 13:36:48 +0200

    Update UpdateScript.ps1

--- UpdateScript.ps1
+++ UpdateScript.ps1
@@ -1,28 +1,42 @@
-﻿# UpdateScript
+
+# UpdateScript
 #
 #
 # 05.06.2020
 # Stian Larsen
 
-
 $isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
 
     if ($isAdmin -eq $False) {
         Write-Host "You need to start this script as admin!" "`n"
+        $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+        Write-host "Your location is $ScriptDir"
         Start-Sleep -Seconds 2
-        $InstallationPath = Get-Location
-        Start-Process powershell -Verb runAs
+       
+        $FilePath = Resolve-Path "UpdateScript.ps1"
+
+        Start-Process powershell -Verb runAs -Command Set-location $Scriptdir | Push-Location $FilePath
+
     }
 
-    Write-host "Updating applications.. Please Wait." -ForegroundColor Yellow
+    $AnswerText = "n";
 
-    choco update all -a --verbose
+    if ($AnswerText -eq $True) {
+        Out-Null
+        Exit;
+    }
+Write-host "Updating applications.. Please Wait." -ForegroundColor Yellow
+
+    choco update all -y --verbose
 
 
     Write-host "Upgrading applications.. Please wait" -ForegroundColor Yellow
-    choco upgrade all -a --verbose 
+
+    choco upgrade all -y --verbose 
    
 
    if ($UpdatedPrograms -eq $True) {
         Out-Null
    }
+
+   Read-host -Prompt "Press Enter to Exit"
